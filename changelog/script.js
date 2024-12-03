40f82400bd3b46e28c7897030be79f1686fed9b5
EdwardVelasquezAntonio <EdwardVelasquez2024.VE@outlook.com>
2024-12-03 14:38:27 -0400

    Delete ChangeLog directory

--- changelog/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('toggle-mode').addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    document.querySelector('header').classList.toggle('dark-mode');
-});
